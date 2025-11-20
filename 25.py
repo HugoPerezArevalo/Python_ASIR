9b50e92b91c819f360766290e7d8ab763595a04b
HugoPerezArevalo <miguelyhugo57@gmail.com>
2025-11-20 18:48:03 +0100

    25

--- 25.py
+++ 25.py
@@ -1,28 +1,56 @@
 
+print("=== MATRÍCULA UNIVERSITARIA ===")
+
 nombre = input("Ingrese el nombre del postulante: ")
-facultad = input("Ingrese la facultad a estudiar (Ingenieria, Medicina, Derecho, Arquitectura): ")
+facultad = input("Ingrese la facultad: ")
+
+
+fac = facultad.lower()
+
+
 importe = 0
 mensualidad = 0
-if facultad == "Ingenieria":
-    importe = 5000
-    mensualidad = 800
-elif facultad == "Medicina":
-    importe = 7000
-    mensualidad = 1200
-elif facultad == "Derecho":
-    importe = 4000
-    mensualidad = 600
-elif facultad == "Arquitectura":
-    importe = 6000
-    mensualidad = 900
-else:
-    print("Facultad no válida.")
-    exit()
-igv = (importe + mensualidad) * 0.18
-monto_final = importe + mensualidad + igv
+
+match fac:
+    case "ing. de sistemas" | "sistemas" | "ingeniería de sistemas":
+        importe = 350
+        mensualidad = 650
+
+    case "derecho":
+        importe = 300
+        mensualidad = 550
+
+    case "ing. naviera" | "naviera" | "ingeniería naviera":
+        importe = 300
+        mensualidad = 500
+
+    case "ing. pesquera" | "pesquera" | "ingeniería pesquera":
+        importe = 310
+        mensualidad = 460
+
+    case "contabilidad":
+        importe = 280
+        mensualidad = 490
+
+    case "administración":
+        importe = 360
+        mensualidad = 520
+
+    case _:
+        print("Facultad no válida")
+        exit()
+
+
+
+subtotal = importe + mensualidad
+igv = subtotal * 0.18
+total_pagar = subtotal + igv
+
+
+print("\n=== RESULTADOS ===")
 print(f"Postulante: {nombre}")
 print(f"Facultad: {facultad}")
-print(f"Importe: S/{importe}")
-print(f"Mensualidad: S/{mensualidad}")
-print(f"IGV (18%): S/{igv}")
-print(f"Monto final a pagar: S/{monto_final}")
+print(f"Importe de matrícula: ${importe}")
+print(f"Mensualidad: ${mensualidad}")
+print(f"IGV (18%): ${igv:.2f}")
+print(f"Monto total a pagar: ${total_pagar:.2f}")
